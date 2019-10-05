b5aa6d6035091c924a306613c198693acd06726b
Ahmad's HP 360 Laptop <ahmadkoudier@ah-t.de>
2019-10-05 13:01:45 +0200

    Check auto update

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var socketIOManager = require("./socket.ioManager/socket-io");
 var os = require("os");
 var socketIOImport = require("./socket.ioManager/socket-io");
 
-console.log("After check pull requst from GIT")
+console.log("Again .. ")
 
 /**
  * first step, opening the HTTP and HTTPS services
